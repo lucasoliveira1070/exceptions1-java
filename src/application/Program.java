7f0cdd1481f7435bad3d7d88fd88572915284363
lucasoliveira1070 <loliveira306@yahoo.com>
2022-02-17 17:29:30 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,51 +12,40 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Room number");
 		int number = sc.nextInt();
-		
+
 		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkin = sdf.parse(sc.next());
-		
+
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkout = sdf.parse(sc.next());
-		
-		if(!checkout.after(checkin)) {
+
+		if (!checkout.after(checkin)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}else {
-			Reservation reservation = new Reservation(number,checkin,checkout);
-			System.out.println("Reservation: "+reservation);
-			
+		} else {
+			Reservation reservation = new Reservation(number, checkin, checkout);
+			System.out.println("Reservation: " + reservation);
+
 			System.out.println();
 			System.out.println("Enter data to update the reservation:");
-			
+
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkin = sdf.parse(sc.next());
-			
+
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkout = sdf.parse(sc.next());
-			
-			Date now = new Date();
-			if(checkin.before (now) || checkout.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+
+			String error = reservation.updateDates(checkin, checkout);
+			if (error != null) {
+				System.out.println(error);
+			} else {
+				System.out.println("Reservation: " + reservation);
 			}
-			else if(!checkout.after(checkin)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
-			else {
-				reservation.updateDates(checkin, checkout);
-				System.out.println("Reservation: "+reservation);
-				
-			}
-			
-			
-			
+
 		}
-		
-		
-		
-		
+
 		sc.close();
 
 	}
